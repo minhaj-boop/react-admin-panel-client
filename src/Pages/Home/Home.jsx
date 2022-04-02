39ac3af915e1eeda6c360defea8eb44e6da03485
minhaj-boop <imminhajj@gmail.com>
2022-04-03 01:09:26 +0600

    initial homepage done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,30 +1,35 @@
-import React from 'react';
-import Chart from '../../Components/Chart/Chart';
-import Featured from '../../Components/Featured/Featured';
-import Navigation from '../../Components/Navigation/Navigation';
-import Sidebar from '../../Components/Sidebar/Sidebar';
-import Widget from '../../Components/Widget/Widget';
-import './Home.scss';
+import React from "react";
+import Chart from "../../Components/Chart/Chart";
+import Featured from "../../Components/Featured/Featured";
+import Navigation from "../../Components/Navigation/Navigation";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import Table from "../../Components/Table/Table";
+import Widget from "../../Components/Widget/Widget";
+import "./Home.scss";
 
 const Home = () => {
-    return (
-        <div className="home">
-            <Sidebar/>
-            <div className="homeContainer">
-                <Navigation/>
-                <div className="widgets">
-                    <Widget type="user"/>
-                    <Widget type="order"/>
-                    <Widget type="earning"/>
-                    <Widget type="balance"/>
-                </div>
-                <div className="charts">
-                    <Featured/>
-                    <Chart/>
-                </div>
-            </div>
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navigation />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
         </div>
-    );
+        <div className="charts">
+          <Featured />
+          <Chart />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <Table />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/Pages/List/List.jsx
+++ src/Pages/List/List.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import './List.scss'
+import React from "react";
+import Datatable from "../../Components/Datatable/Datatable";
+import Navigation from "../../Components/Navigation/Navigation";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import "./List.scss";
 const List = () => {
-    return (
-        <div>
-            <h1>This is list</h1>
-        </div>
-    );
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navigation />
+        <Datatable />
+      </div>
+    </div>
+  );
 };
 
 export default List;
