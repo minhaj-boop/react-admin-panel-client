4d10f8f21574eb8952b6441dddd3a423e26b29d8
minhaj-boop <imminhajj@gmail.com>
2022-04-05 01:41:40 +0600

    users page done

--- src/Pages/Single/Single.jsx
+++ src/Pages/Single/Single.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import './SIngle.scss';
+import React from "react";
+import Navigation from "../../Components/Navigation/Navigation";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import "./SIngle.scss";
 const Single = () => {
-    return (
-        <div>
-            <h1>This is single</h1>
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navigation />
+        <div className="top">
+          <div className="left">
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                alt=""
+                className="itemImg"
+              />
+            </div>
+          </div>
+          <div className="right"></div>
         </div>
-    );
+        <div className="bottom"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Single;
