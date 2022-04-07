2d65b5c91f0572fd36f3721b900318f144fbf4c2
minhaj-boop <imminhajj@gmail.com>
2022-04-07 22:02:35 +0600

    new user started

--- src/Pages/New/New.jsx
+++ src/Pages/New/New.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
-import './New.scss'
+import React from "react";
+import Navigation from "../../Components/Navigation/Navigation";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import "./New.scss";
 const New = () => {
-    return (
-        <div>
-            <h1>This is new</h1>
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navigation />
+        <div className="top">
+          <h1>Add new user</h1>
         </div>
-    );
+        <div className="bottom">
+          <div className="left">
+            <img
+              src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form action="">
+              <div className="formInput">
+                <label>Username</label>
+                <input type="text" placeholder="john_doe" />
+              </div>
+              <div className="formInput">
+                <label>Name and surname</label>
+                <input type="text" placeholder="John Doe" />
+              </div>
+              <div className="formInput">
+                <label>Email</label>
+                <input type="email" placeholder="john_doe@gmail.com" />
+              </div>
+              <div className="formInput">
+                <label>Phone</label>
+                <input type="text" placeholder="+1234567891011" />
+              </div>
+              <div className="formInput">
+                <label>Password</label>
+                <input type="password" />
+              </div>
+              <div className="formInput">
+                <label>Address</label>
+                <input type="text" placeholder="Nowhere street" />
+              </div>
+              <div className="formInput">
+                <label>Country</label>
+                <input type="text" placeholder="Nowhere" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default New;
